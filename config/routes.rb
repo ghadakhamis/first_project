a3e10c1bef78f4217b2c888b181b922094da079f
ghadakhamis <ghada.khamis.m@gmail.com>
2018-05-04 20:25:07 +0200

    change routes to home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/home', 'welcome#home'
-  get 'welcome/about','welcome#about'
+  root 'welcome#home'
+  get 'about',to: 'welcome#about'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
